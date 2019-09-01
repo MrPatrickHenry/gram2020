ecbedfbabfccab53c41e68d336ff23ee80fdfdbd
Patrick Henry <patrick@gomonsterful.com>
2019-09-01 14:48:02 -0600

    update template

--- source/app/resources/views/layouts/app.blade.php
+++ source/app/resources/views/layouts/app.blade.php
@@ -10,14 +10,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://20-twenty.online/js/app.js" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://20-twenty.online/css/app.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
