aecd1e60e1e4e710d8561ba816fa089c3e161e22
Patrick Henry <patrick@gomonsterful.com>
2019-09-02 11:22:32 -0600

    register

--- source/app/resources/views/auth/register.blade.php
+++ source/app/resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action="https://20-twenty.online/reigster">
                         @csrf
 
                         <div class="form-group row">
