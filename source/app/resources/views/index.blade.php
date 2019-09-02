68805c3262bbe1a1e15775a2f3adfce6cfac609c
Patrick Henry <patrick@gomonsterful.com>
2019-09-02 12:18:56 -0600

    misse the end if

--- source/app/resources/views/index.blade.php
+++ source/app/resources/views/index.blade.php
@@ -5,6 +5,5 @@
             <a href="https://api.instagram.com/oauth/authorize/?client_id=16b479bb667f424fb1bcb1c8dae8cf9f&redirect_uri=https://20-twenty.online/instagram/callback/&response_type=code" class="btn btn-default">
                 Authenticate Instagram
             </a>
-        @endif
     </div>
 @endsection
