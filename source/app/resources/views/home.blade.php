3a0a0ef4a845e725c504b44db9f798ead5b22b4a
Patrick Henry <patrick@gomonsterful.com>
2019-09-02 13:22:24 -0600

    re-attache buttons

--- source/app/resources/views/home.blade.php
+++ source/app/resources/views/home.blade.php
@@ -15,6 +15,12 @@
                     @endif
 
                     You are logged in!
+
+                    <div class="container app">
+            <a href="https://api.instagram.com/oauth/authorize/?client_id=16b479bb667f424fb1bcb1c8dae8cf9f&redirect_uri=https://20-twenty.online/instagram/callback/&response_type=code" class="btn btn-default">
+                Authenticate Instagram
+            </a>
+    </div>
                 </div>
             </div>
         </div>
