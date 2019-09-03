7adf041a0412836475f85fb071b4257c5a7d4ea8
Patrick Henry <patrick@gomonsterful.com>
2019-09-03 13:57:00 -0600

    this

--- source/app/app/Http/Controllers/InstagramController.php
+++ source/app/app/Http/Controllers/InstagramController.php
@@ -84,8 +84,7 @@ class InstagramController extends Controller
     $varDump = round($EngagementRate = ($likes + $comments) / $igFollowedBy * 100 / 10 ,2);
     $data = array ($likes, $comments, $igFollowedBy, $varDump );
 
-    store($likes, $igFollowedB, $igMediaPosts, $engagementRate);
-
+return $this->store($likes, $igFollowedB, $igMediaPosts, $engagementRate);
 
 }
 
