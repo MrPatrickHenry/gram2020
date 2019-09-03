b7001b29888d33b7b0aafb0b55ea3d815c74688e
Patrick Henry <patrick@gomonsterful.com>
2019-09-03 13:47:21 -0600

    gram table and stor

--- source/app/database/migrations/2019_09_03_191051_create_ig_table.php
+++ source/app/database/migrations/2019_09_03_191051_create_ig_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateIgTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('ig', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('access_token');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->integer('userID');
+            $table->integer('likes');
+            $table->integer('followers');
+            $table->integer('engagementRate');
         });
     }
 
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('ig');
     }
 }
